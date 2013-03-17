4b20b0ac95bd5c40b42dc2f7b5cf7824cd65af1a
ynohat <anthony@hogg.fr>
2013-03-17 17:26:24 +0100

    added example: select with aliased columns

--- examples/_select.js
+++ examples/_select.js
@@ -34,6 +34,38 @@ module.exports = {
             js: [
                 'sql.select("id").from("recipe").where(sql.$("id IN (?)", [1, 2, 3, 4]))'
             ]
+        },
+
+        {
+            comment: 'select * from recipe join recipe_ingredient join ingredient',
+            js: [
+                'sql.select().from("recipe")',
+                '   .joinLeft("recipe_ingredient", "recipe.id = recipe_ingredient.recipe_id")',
+                '   .joinLeft("ingredient", "recipe_ingredient.ingredient_id = ingredient.id")'
+            ]
+        },
+
+        {
+            comment: 'select * from recipe join recipe_ingredient join ingredient, using aliased table names',
+            js: [
+                'sql.select().from({"r": "recipe"})',
+                '   .joinLeft({"ri": "recipe_ingredient"}, "r.id = ri.recipe_id")',
+                '   .joinLeft({"i": "ingredient"}, "ri.ingredient_id = i.id")'
+            ]
+        },
+
+        {
+            comment: 'the same, using aliased columns',
+            js: [
+                'sql.select({',
+                '   "recipe_id": "r.id",',
+                '   "recipe_name": "r.name",',
+                '   "ingredient_id": "i.id",',
+                '   "ingredient_name": "i.name"',
+                '}).from({"r": "recipe"})',
+                '.joinLeft({"ri": "recipe_ingredient"}, "r.id = ri.recipe_id")',
+                '.joinLeft({"i": "ingredient"}, "ri.ingredient_id = i.id")'
+            ]
         }
     ]
 };
